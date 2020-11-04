4de072742e3eb2ef6274bf094e431f404afd5f06
Dur E Samin <bitf17a545@pucit.edu.pk>
2020-11-04 13:36:08 +0500

    quranDATA

---
+++ src/main/java/com/example/quran_data/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.quran_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String []data =getResources().getStringArray(R.array.translation);
+        ArrayList<String> arrayList = new ArrayList<String>(asList(data));
+         ArrayAdapter adapter  = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList );
+         ListView  listView = (ListView)findViewById(R.id.simpleListView );
+        listView.setAdapter((adapter));
+       // setContentView(R.layout);
+       // final String input = et.getText().toString();
+    }
+}
